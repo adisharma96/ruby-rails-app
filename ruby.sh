b8cfe88c5d253785a4924f3495c5c6d8dd5ea0fc
root <root@ip-172-31-14-209.eu-central-1.compute.internal>
2022-02-19 18:38:56 +0000

    added ruby.sh

--- ruby.sh
+++ ruby.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+if [ "-d tmp/db" ]
+then
+echo "Old DB removed: $(rm -rf tmp/db)"
+else
+echo "No DB exist"
+fi
+
+if [ "-f tmp/pids/server.pid" ]
+then
+echo "server.pid removed : $(rm -rf tmp/pids/server.pid)"
+else
+echo "server.pid not found"
+fi
+
 docker-compose up -d
 a=$(docker ps | awk 'NR > 1 {print $1; exit}')
 echo $a
