6d773e52beb01a110b0847820b3223b4bf5828ef
adisharma96 <adisharma96@gmail.com>
2022-02-20 01:19:36 +0530

    Update ruby.sh

--- ruby.sh
+++ ruby.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 if [ "-d tmp/db" ]
 then
-echo "Old DB removed: $(rm -rf tmp/db)"
+echo "Old DB removed: $(sudo rm -rf tmp/db)"
 else
 echo "No DB exist"
 fi
 
 if [ "-f tmp/pids/server.pid" ]
 then
-echo "server.pid removed : $(rm -rf tmp/pids/server.pid)"
+echo "server.pid removed : $(sudo rm -rf tmp/pids/server.pid)"
 else
 echo "server.pid not found"
 fi
 
-docker-compose up -d
-a=$(docker ps | awk 'NR > 1 {print $1; exit}')
+sudo docker-compose up -d
+sudo a=$(sudo docker ps | awk 'NR > 1 {print $1; exit}')
 echo $a
-echo "$(sleep 5)"
-docker exec -it $a rails db:create
-docker exec -it $a rails db:migrate
+#echo "$(sleep 5)"
+sudo docker exec -it $a rails db:create
+sudo docker exec -it $a rails db:migrate
